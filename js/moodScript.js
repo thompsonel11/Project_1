a3b44814368e09c51f1a47e36159bea376f4cbf9
Liz Thompson <thompsonel2011@gmail.com>
2021-05-08 13:00:50 -0400

    GIFs working

--- js/moodScript.js
+++ js/moodScript.js
@@ -1,10 +1,35 @@
-// APIs FOR MOODS 
+// API FOR MOODS 
+var mood = ['sad', 'happy', 'relaxed', 'reckless', 'flirty', 'angry', 'lonely', 'celebrate']
 
-var mood = [sad, happy, relaxed, reckless, flirty, angry, lonely, celebrate]
+for (let i = 0; i < mood.length; i++) {
+
+    
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://api.giphy.com/v1/gifs/search?api_key=H76z6WrcWkxdfruPUsbzRHEWsM4t19DK&q=${mood[i]}`,
+        "method": "GET",
+    }
+
+    var randomGifNumber = 5 // math.random on 50 results
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        var moodImg = $(`#${mood[i]}`)
+        console.log('mood img ', moodImg)
+        moodImg.attr('src', response.data[randomGifNumber].images.original.url)
+    });
+
+
+    
+    
+}
+// API FOR LIQUOR TYPE
+var liquorType = ['vodka', 'gin', 'whiskey', 'liqueur', 'rum', 'tequila', 'brandy', 'jagermeister']
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": `https://api.giphy.com/v1/gifs/search?api_key=H76z6WrcWkxdfruPUsbzRHEWsM4t19DK&q=${mood}`,
+    "url": `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${liquorType}`,
     "method": "GET",
   }
 
