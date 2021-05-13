7516d99f38be26631762505674b17ca46a4e10d6
lissy <slissy513@gmail.com>
2021-05-13 13:22:33 -0400

    added spinner wheel and function to hide containers when cocktaail is rendered

--- js/random.js
+++ js/random.js
@@ -1,14 +1,40 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://the-cocktail-db.p.rapidapi.com/random.php",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "eb0118c1d1mshee2be64239a8bccp1b9027jsn78e29b72b859",
-		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
-	}
-};
+var random = document.getElementById('random')
+var results = document.getElementById('results')
+var drinkResult;
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+$(document).on('click', '.spin', function () {
+	random.classList.add('hide')
+	results.classList.remove('hide')
+
+})
+
+	var typeSearch = {
+		"async": true,
+		"crossDomain": true,
+		"url": `https://www.thecocktaildb.com/api/json/v1/1/random.php?i=${random}`,
+		"method": "GET",
+	}
+	
+	$.ajax(typeSearch).done(function (response) {
+		console.log(response)
+	})
+
+
+
+	//Spinner function
+	function myfunction(){
+
+
+		var x = 1024; //min value
+		var y = 9999; // max value
+	
+		var deg = Math.floor(Math.random() * (x - y)) + y;
+	
+		document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+	
+		var element = document.getElementById('mainbox');
+		element.classList.remove('animate');
+		setTimeout(function(){
+			element.classList.add('animate');
+		}, 5000); //5000 = 5 second
+	}
